0c26457cb3772495e5a3b30cba5257b04ac0c2b6
thunder609 <oswaldo-pachco@hotmail.es>
2023-03-31 22:38:07 -0400

    update5

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import { create, router as _router, defaults } from 'json-server'
+const jsonServer = require('json-server')
 
-const server = create()
+const server = jsonServer.create()
 
-const router = _router('data.json')
-const middlewares = defaults()
+const router = jsonServer.router('data.json')
+const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use(router)
